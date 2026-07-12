2724bc267ff90a56bec853eae0d8714d1a5df5dd
BrokznAngzl <bukobeer001@gmail.com>
2026-07-12 23:37:03 +0700

    fix import

--- src/main/java/open/microservice/accountmanagement/repository/profile/AddressRepository.java
+++ src/main/java/open/microservice/accountmanagement/repository/profile/AddressRepository.java
@@ -1,6 +1,7 @@
 package open.microservice.accountmanagement.repository.profile;
 
-import com.open.project.account.models.hibernate.pf.Address;
+
+import open.microservice.accountmanagement.model.hibernate.pf.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
