a91b5e6cfbdd5f693e6f699113739cc78500ba89
BrokznAngzl <bukobeer001@gmail.com>
2026-07-19 01:02:03 +0700

    add err constant

---
+++ src/main/java/open/microservice/accountmanagement/constant/ErrorConstant.java
@@ -0,0 +1,15 @@
+package open.microservice.accountmanagement.constant;
+
+public class ErrorConstant {
+    public static final String VALIDATE_FAILED = "Validation Failed";
+    public static final String RESOURCE_NOT_FOUND = "Resource Not Found";
+    public static final String RESOURCE_NOT_FOUND_DETAIL = "Resource Not Found {0}: {1}";
+    public static final String COMPOSE_FAILED = "Compose Failed";
+    public static final String COMPOSER_NOT_FOUND_DETAIL = "Composer Not Found: {0}";
+    public static final String EXTERNAL_ORDER_NOT_FOUND_DETAIL = "External Order Not Found: {0}";
+    public static final String UNPEXPECTED_ERROR = "An unexpected error occurred";
+
+    public static final String ER404 = "ERR-404";
+    public static final String ER400 = "ERR-400";
+
+}
