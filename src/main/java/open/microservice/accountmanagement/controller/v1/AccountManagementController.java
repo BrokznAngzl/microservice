f10cf80aad85c89f72d346f9b22e182efef92842
BrokznAngzl <bukobeer001@gmail.com>
2026-07-19 01:01:31 +0700

    clean code

--- src/main/java/open/microservice/accountmanagement/controller/v1/AccountManagementController.java
+++ src/main/java/open/microservice/accountmanagement/controller/v1/AccountManagementController.java
@@ -2,9 +2,9 @@ package open.microservice.accountmanagement.controller.v1;
 
 
 
-import open.microservice.accountmanagement.controller.CreateAccountHelper;
+import open.microservice.accountmanagement.controller.CreateAccountWorkFlow;
 import open.microservice.accountmanagement.model.hibernate.OrderPropertyInformation;
-import open.microservice.accountmanagement.model.request.AccountRequest;
+import open.microservice.accountmanagement.model.request.internal.AccountRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +15,21 @@ import static open.microservice.accountmanagement.constant.APIConstant.*;
 @RequestMapping(SERVICE_PREFIX_V1)
 public class AccountManagementController {
     @Autowired
-    CreateAccountHelper createAccountHelper;
+    CreateAccountWorkFlow createAccountWorkFlow;
 
     @PostMapping(value = REGISTRY)
-    public ResponseEntity<?> createNewAccount(@RequestBody AccountRequest requestBody) throws Exception {
-        createAccountHelper.validateRequest(requestBody);
-        OrderPropertyInformation orderProperty = createAccountHelper.buildOrderProperty(requestBody);
-        createAccountHelper.composeExOrderAndParam(orderProperty);
-        createAccountHelper.saveOrderAndExOrder(orderProperty.getOrder());
-        createAccountHelper.provisioning(orderProperty.getOrder());
-        ResponseEntity<?> responseBody = createAccountHelper.composeResponse(orderProperty, requestBody);
-        return responseBody;
+    public ResponseEntity<?> createNewAccount(@RequestBody AccountRequest requestBody) throws Exception{
+        createAccountWorkFlow.validateRequest(requestBody);
+
+        OrderPropertyInformation orderProperty = createAccountWorkFlow.buildOrderProperty(requestBody);
+
+        createAccountWorkFlow.composeExOrderAndParam(orderProperty);
+
+        createAccountWorkFlow.saveOrderAndExOrder(orderProperty.getOrder());
+
+        createAccountWorkFlow.provisioning(orderProperty.getOrder());
+
+        return createAccountWorkFlow.composeResponse(orderProperty, requestBody);
     }
 
 //    @PatchMapping(value = MODIFY)
@@ -35,7 +39,7 @@ public class AccountManagementController {
 
 //    @PostMapping(value = RESEND)
 //    public ResponseEntity<?> resendExternalOrder(@RequestBody AccountRequest requestBody) throws Exception {
-//        ResponseEntity<?> responseBody = createAccountHelper.composeResponse(new OrderBufferModel(), requestBody);
+//        ResponseEntity<?> responseBody = createAccountWorkFlow.composeResponse(new OrderBufferModel(), requestBody);
 //        return responseBody;
 //    }
 
