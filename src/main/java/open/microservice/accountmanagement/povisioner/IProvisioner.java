f4474dea3dc895073d264ea1abe8dd70d5b90b69
BrokznAngzl <bukobeer001@gmail.com>
2026-07-23 14:45:13 +0700

    add feature re-provisioning flow

--- src/main/java/open/microservice/accountmanagement/povisioner/IProvisioner.java
+++ src/main/java/open/microservice/accountmanagement/povisioner/IProvisioner.java
@@ -5,10 +5,8 @@ import open.microservice.accountmanagement.model.hibernate.om.ExternalOrder;
 
 public interface IProvisioner {
 
-    public boolean canProvisioning(String value);
+    boolean canProvisioning(String value);
 
-    public void provisioning(ExternalOrder externalOrder);
-
-    public void resend(ExternalOrder externalOrder);
+    void provisioning(ExternalOrder externalOrder);
 
 }
